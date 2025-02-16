6ed0452e3333da9c1d374b2948386ff8106e3d78
ventsze <zwz614371723@icloud.com>
2025-02-16 17:54:13 +0300

    初始化项目

---
+++ test_app.py
@@ -0,0 +1,15 @@
+# test_add.py
+import unittest
+from app import add
+
+class TestApp(unittest.TestCase):
+    def test_add(self):
+        self.assertEqual(add(2, 3), 5)
+        self.assertEqual(add(-1, 1), 0)
+        self.assertEqual(add(-2, -3), -5)
+
+    def test_add2(self):
+        self.assertEqual(1 + 1, 2)
+
+if __name__ == '__main__':
+    unittest.main()
